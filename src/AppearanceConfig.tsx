2e4c7c8f8bbfd44104889f3ef8bfcf14ff687152
Anton Novikov <tonn.post@gmail.com>
2020-10-30 12:12:27 +0400

    feat: more useful color selector

--- src/AppearanceConfig.tsx
+++ src/AppearanceConfig.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { HexColorPicker } from 'react-colorful';
+import React, { useReducer } from 'react';
+import { SliderPicker } from 'react-color';
+import './AppearanceConfig.scss';
 
 export const DefaultAppearance: IAppearance = {
   color: '#222',
@@ -11,17 +12,22 @@ export const DefaultAppearance: IAppearance = {
 };
 
 export interface IAppearance {
-    fontSize: number;
-    fontFamily: string;
-    lineHeight: number;
-    background: string;
-    padding: number[];
-    color: string;
+  fontSize: number;
+  fontFamily: string;
+  lineHeight: number;
+  background: string;
+  padding: number[];
+  color: string;
 }
 
 export const AppearanceConfig: React.FC<{ Appearance: IAppearance, onChanged: () => void }> = ({ Appearance, onChanged }) => {
-    return (<>
-        <HexColorPicker color={Appearance.background} onChange={color => { Appearance.background = color; onChanged(); } } />
-        <HexColorPicker color={Appearance.color} onChange={color => { Appearance.color = color; onChanged(); } } />
-    </>);
+  const [, forceUpdate] = useReducer((x: number) => x + 1, 0);
+
+  return (<>
+    Background color:
+    <SliderPicker color={Appearance.background}  onChange={color => { Appearance.background = color.hex; onChanged(); forceUpdate(); } }/>
+    <br/>
+    Font color:
+    <SliderPicker color={Appearance.color} onChange={color => { Appearance.color = color.hex; onChanged(); forceUpdate(); } }/>
+  </>);
 }
